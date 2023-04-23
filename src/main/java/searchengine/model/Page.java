504452918d8c05c982bd27c8844873664de08491
Алексей Романов <baxeon96@yandex.ru>
2023-04-23 13:23:58 +0300

    added db migration with liquibase

---
+++ src/main/java/searchengine/model/Page.java
@@ -0,0 +1,26 @@
+package searchengine.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class Page {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "site_id", nullable = false)
+    private Site site;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String path;
+
+    @Column(nullable = false)
+    private int code;
+
+    @Column(columnDefinition = "MEDIUMTEXT", nullable = false)
+    private String content;
+}
--- src/main/java/searchengine/model/Site.java
+++ src/main/java/searchengine/model/Site.java
@@ -2,25 +2,41 @@ package searchengine.model;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
+@Table
 public class Site {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    //@OneToMany
-    int id;
+    @Column(nullable = false)
+    private int id;
 
-    @Column(columnDefinition = "ENUM('INDEXING','INDEXED','FAILED')")
-    String status;
+    @Column(columnDefinition = "ENUM('INDEXING','INDEXED','FAILED')", nullable = false)
+    private SiteStatus status;
 
-    @Column(name = "status_time")
-    Timestamp statusTime;
+    @Column(name = "status_time", nullable = false)
+    private Timestamp statusTime;
 
-    @Column(name = "last_error")
-    String lastError;
+    @Column(name = "last_error", columnDefinition = "TEXT")
+    private String lastError;
 
-    String url;
+    @Column(columnDefinition = "VARCHAR(255)", nullable = false)
+    private String url;
 
-    String name;
+    @Column(columnDefinition = "VARCHAR(255)", nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "site")
+    private Set<Page> pages = new HashSet<>();
+
+    /* @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "index",
+            joinColumns = {@JoinColumn(name = "site_id")},
+            inverseJoinColumns = {@JoinColumn(name = "lemma_id")}
+    )
+    List<Lemma> lemmas = new ArrayList<>(); */
 }
